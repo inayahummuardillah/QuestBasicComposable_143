0030479059190e134d923286d13d80f3295f43d6
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2024-10-17 16:39:00 +0630

    Disini telah membuat fungsi composable BasicLayout dengan tata letak Column yang terpusat.

--- MainActivity.kt
+++ MainActivity.kt
@@ -40,6 +40,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicLayout(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+    }
 }
 
 @Preview(showBackground = true)
